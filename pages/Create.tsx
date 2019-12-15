43094014aa72c2ca70b80e590c37659eade629be
Orlyanskaya Anna <annorl.em@gmail.com>
2019-12-15 18:55:31 +0300

    форма1

--- pages/Create.tsx
+++ pages/Create.tsx
@@ -2,43 +2,49 @@ import React from 'react'
 import withAppLayout from '../components/AppLayout/Layout'
 import Lightrope from '../components/Lightrope/Lightrope'
 import Hohoho from '../components/Hohoho/Hohoho'
-import {Form, Grid, Segment} from 'semantic-ui-react'
+import {Form, Segment} from 'semantic-ui-react'
+import Container from '../components/Container/Container'
+import styles from './Create.module.css'
 
 const Create = () => {
-    return <>
+    return <div className={styles.root}>
         <Lightrope/>
-        <div style={{width: 500, margin: '24px auto'}}>
-            <Segment>
-                <Form>
-                    <Form.Input fluid label='Название' placeholder='Название'/>
+        <Container>
+            <div className={styles.form_container}>
+            <div className={styles.form}>
+                <Segment>
+                    <Form>
+                        <Form.Input fluid label='Название' placeholder='Название'/>
 
-                    <Form.Group inline>
-                        <label>Максимальная сумма</label>
+                        <Form.Group inline>
+                            <label>Максимальная сумма</label>
+                            <Form.Radio
+                                label='1000'
+                                value='1000'
+                            />
+                            <Form.Radio
+                                label='2000'
+                                value='2000'
+                            />
+                            <Form.Radio
+                                label='3000'
+                                value='3000'
+                            />
+                        </Form.Group>
                         <Form.Radio
-                            label='1000'
-                            value='1000'
+                            label='Свой вариант'
+                            value='custom'
                         />
-                        <Form.Radio
-                            label='2000'
-                            value='2000'
-                        />
-                        <Form.Radio
-                            label='3000'
-                            value='3000'
-                        />
-                    </Form.Group>
-                    <Form.Radio
-                        label='Свой вариант'
-                        value='custom'
-                    />
-                    <Form.Input placeholder='Сумма' width={5}/>
-                    <Form.TextArea label='Описание' placeholder='Описание'/>
-                    <Form.Button color="red">Создать</Form.Button>
-                </Form>
-            </Segment>
-        </div>
+                        <Form.Input placeholder='Сумма' width={5}/>
+                        <Form.TextArea label='Описание' placeholder='Описание'/>
+                        <Form.Button color="red">Создать</Form.Button>
+                    </Form>
+                </Segment>
+            </div>
+            </div>
+        </Container>
         <Hohoho small/>
-    </>
+    </div>
 }
 
 
