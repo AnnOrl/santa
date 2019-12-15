1ce5a5f2e922ff787e436d996505f3b02c938f60
Orlyanskaya Anna <annorl.em@gmail.com>
2019-12-15 23:22:11 +0300

    prod

--- server/index.ts
+++ server/index.ts
@@ -59,6 +59,8 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(process.env.PORT || 3000);
+        server.listen(process.env.PORT || 3000, () => {
+            console.log(`Server started on port ${process.env.PORT || 3000}!`);
+        });
     }).catch(error => console.log(error));
 
